d3357a0624206615bb2c418a52ea392b4b969495
sonnym <michaud.sonny@gmail.com>
2013-12-26 17:51:04 -0500

    add and configure connect-mincer middleware

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require("http");
+var path = require("path");
 
 var express = require("express");
 var sockjs = require("sockjs");
+
+var ConnectMincer = require("connect-mincer");
 
 var SocketHandler = require("./lib/socket_handler");
 
@@ -10,6 +13,18 @@ var port = (isProduction ? 80 : 8000);
 var sockjs_opts = { sockjs_url: "http://cdn.sockjs.org/sockjs-0.3.4.min.js" }
 
 var app = express();
+
+var connectMincer = new ConnectMincer({
+  root: __dirname,
+  production: false,
+  mountPoint: "/assets",
+  manifestFile: path.join(__dirname, "public/assets/manifest.json"),
+  paths: [ "lib/assets/js" ]
+});
+
+app.use(connectMincer.assets());
+app.use("/assets", connectMincer.createServer());
+
 var socket = sockjs.createServer(sockjs_opts);
 
 app.use(express.static(__dirname + '/public'));
