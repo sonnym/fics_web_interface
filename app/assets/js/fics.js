db2184e8f1d9affd78642b549c5c3809e9b82ca7
sonnym <michaud.sonny@gmail.com>
2013-12-31 15:42:55 -0500

    update logged in status via watch

--- app/assets/js/fics.js
+++ app/assets/js/fics.js
@@ -1,9 +1,9 @@
 var ficsClient = angular.module("ficsClient", ["ui.bootstrap.buttons", "ui.bootstrap.pagination", "ui.bootstrap.tabs"]);
 
 ficsClient.run(["$rootScope", "User", function($rootScope, User) {
-  $rootScope.isLoggedIn = function() {
-    return angular.isDefined(User.getUsername());
-  }
+  $rootScope.$watch(User.getUsername, function(username) {
+    $rootScope.isLoggedIn = angular.isDefined(username);
+  }, true);
 
   $rootScope.isGuest = User.isGuest;
 }]);
