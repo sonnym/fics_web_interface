b0584be296f9e61fd7df1e1dd8d22249552b0087
sonnym <michaud.sonny@gmail.com>
2013-12-30 23:38:13 -0500

    expose angular-ui-bootstrap buttons

--- app/assets/js/fics.js
+++ app/assets/js/fics.js
@@ -1,4 +1,4 @@
-var ficsClient = angular.module("ficsClient", ["ui.bootstrap.pagination", "ui.bootstrap.tabs"]);
+var ficsClient = angular.module("ficsClient", ["ui.bootstrap.buttons", "ui.bootstrap.pagination", "ui.bootstrap.tabs"]);
 
 ficsClient.run(["$rootScope", "User", function($rootScope, User) {
   $rootScope.isLoggedIn = function() {
