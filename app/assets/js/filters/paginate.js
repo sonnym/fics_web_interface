4f5676ed32d775f437b07c5ac7fa8edabd92bb02
sonnym <michaud.sonny@gmail.com>
2014-01-01 14:18:39 -0500

    fix reference to undefined

--- app/assets/js/filters/paginate.js
+++ app/assets/js/filters/paginate.js
@@ -1,5 +1,9 @@
 ficsClient.filter("paginationSubset", function() {
   return function(items, perPage, currentPage) {
+    if (angular.isUndefined(items)) {
+      return [];
+    }
+
     return items.splice(((currentPage || 1) - 1) * perPage, perPage);
   }
 });
