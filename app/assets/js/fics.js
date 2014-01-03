aed1bf143d463c774da6eca343d27bbee6168b62
sonnym <michaud.sonny@gmail.com>
2014-01-03 10:08:37 -0500

    add ui-utils modules

--- app/assets/js/fics.js
+++ app/assets/js/fics.js
@@ -1,4 +1,5 @@
-var ficsClient = angular.module("ficsClient", ["ui.bootstrap.buttons", "ui.bootstrap.dropdownToggle", "ui.bootstrap.pagination", "ui.bootstrap.tabs"]);
+var ficsClient = angular.module("ficsClient", ["ui.bootstrap.buttons", "ui.bootstrap.dropdownToggle", "ui.bootstrap.pagination", "ui.bootstrap.tabs",
+                                               "ui.keypress", "ui.scroll"]);
 
 ficsClient.run(["$rootScope", "User", function($rootScope, User) {
   $rootScope.$watch(User.getUsername, function(username) {
