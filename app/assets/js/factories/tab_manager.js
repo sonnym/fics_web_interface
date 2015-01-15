f1388234432d39f38a77ff0d5bb8c1966bc39465
sonnym <michaud.sonny@gmail.com>
2015-01-14 21:41:21 -0500

    move knowledge of available tabs out of manager

--- app/assets/js/factories/tab_manager.js
+++ app/assets/js/factories/tab_manager.js
@@ -1,31 +1,41 @@
 ficsClient.factory("TabManager", ["$timeout", function($timeout) {
-  return function(scope) {
-    scope.tabs = { login: true, console: false, chat: false, play: false, watch: false };
+  return function(scope, tabs) {
+    Object.defineProperty(this, "tabs", {
+      enumerable: true,
+      get: function() {
+        return tabs;
+      },
+    });
 
     scope.changeTab = function(activeTab) {
-      scope.tabs = _.reduce(scope.tabs, function(obj, active, tab) {
-        obj[tab] = tab === activeTab;
+      tabs = _.reduce(tabs, function(newTabs, tabData, tabName) {
+        newTabs[tabName] = _.extend(tabData, {
+          active: (tabName === activeTab)
+        })
 
-        return obj;
+        return newTabs;
       }, {});
     };
 
-    scope.isActiveTab = function(tab) {
-      return tab === activeTab();
-    };
-
-    return {
-      watchLogin: function() {
-        if (scope.isLoggedIn && scope.isActiveTab("login")) {
-          // next run of the event loop, since the changing tabs
-          // cause the last tab to become active by default
-          $timeout(_.partial(_.bindAll(scope, "changeTab").changeTab, "chat"));
-        }
+    var self = this;
+    this.watchLogin = function() {
+      if (scope.isLoggedIn && isActiveTab("login")) {
+        // next run of the event loop, since the changing tabs
+        // cause the last tab to become active by default
+        $timeout(_.partial(_.bindAll(scope, "changeTab").changeTab, "chat"));
       }
     };
 
-    function activeTab() {
-      return _.invert(scope.tabs)[true];
+    function activeTabName() {
+      return _.invert(_.reduce(tabs, function(memo, tabData, tabName) {
+        memo[tabName] = tabData.active;
+
+        return memo;
+      }, {}))[true];
     }
+
+    function isActiveTab(tabName) {
+      return tabName === activeTabName();
+    };
   };
 }]);
--- app/assets/js/fics.js
+++ app/assets/js/fics.js
@@ -11,7 +11,15 @@ var ficsClient = angular.module("ficsClient", [
 
 // only include play as a dependency temporarily to make it register its messages
 ficsClient.run(["$rootScope", "TabManager", "User", "Play", function($rootScope, TabManager, User) {
-  var tabManager = new TabManager($rootScope);
+  var tabManager = new TabManager($rootScope, {
+    login: { active: true },
+    console: { active: false },
+    chat: { active: false },
+    play: { active: false },
+    watch: { active: false }
+  });
+
+  $rootScope.tabs = tabManager.tabs;
 
   $rootScope.$watch(User.getUsername, function(username) {
     $rootScope.isLoggedIn = angular.isDefined(username);
