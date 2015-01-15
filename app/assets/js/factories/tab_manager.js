f371762cc796308a1e5c3fe98c7803a7f07ec0db
sonnym <michaud.sonny@gmail.com>
2015-01-14 19:44:27 -0500

    automatically attach tab manager to scope

--- app/assets/js/factories/tab_manager.js
+++ app/assets/js/factories/tab_manager.js
@@ -1,22 +1,20 @@
 ficsClient.factory("TabManager", ["$timeout", function($timeout) {
   return function(scope) {
+    scope.tabs = { login: true, console: false, chat: false, play: false, watch: false };
+
+    scope.changeTab = function(activeTab) {
+      scope.tabs = _.reduce(scope.tabs, function(obj, active, tab) {
+        obj[tab] = tab === activeTab;
+
+        return obj;
+      }, {});
+    };
+
+    scope.isActiveTab = function(tab) {
+      return tab === activeTab();
+    };
+
     return {
-      attach: function() {
-        scope.tabs = { login: true, console: false, chat: false, play: false, watch: false };
-
-        scope.changeTab = function(activeTab) {
-          scope.tabs = _.reduce(scope.tabs, function(obj, active, tab) {
-            obj[tab] = tab === activeTab;
-
-            return obj;
-          }, {});
-        };
-
-        scope.isActiveTab = function(tab) {
-          return tab === activeTab();
-        };
-      },
-
       watchLogin: function() {
         if (scope.isLoggedIn && scope.isActiveTab("login")) {
           // next run of the event loop, since the changing tabs
@@ -24,7 +22,7 @@ ficsClient.factory("TabManager", ["$timeout", function($timeout) {
           $timeout(_.partial(_.bindAll(scope, "changeTab").changeTab, "chat"));
         }
       }
-    }
+    };
 
     function activeTab() {
       return _.invert(scope.tabs)[true];
--- app/assets/js/fics.js
+++ app/assets/js/fics.js
@@ -12,7 +12,6 @@ var ficsClient = angular.module("ficsClient", [
 // only include play as a dependency temporarily to make it register its messages
 ficsClient.run(["$rootScope", "TabManager", "User", "Play", function($rootScope, TabManager, User) {
   var tabManager = new TabManager($rootScope);
-  tabManager.attach();
 
   $rootScope.$watch(User.getUsername, function(username) {
     $rootScope.isLoggedIn = angular.isDefined(username);
