5ea1a7df8a1b6de4454a682678307bcf239077a5
sonnym <michaud.sonny@gmail.com>
2015-01-20 10:38:47 -0500

    add factory for encapsulating message collections
    extending the existing activity notifier factory

---
+++ app/assets/js/factories/message_collection.js
@@ -0,0 +1,30 @@
+ficsClient.factory("MessageCollection", ["FiniteArray", "ActivityNotifier", function(FiniteArray, ActivityNotifier) {
+  function MessageCollection(message) {
+    this.messages = new FiniteArray();
+    this.newMessages = false;
+
+    if (message) {
+      this.push(message)
+    }
+
+    var self = this;
+    ActivityNotifier.call(this, {
+      active: false,
+
+      activate: function() {
+        self.newMessages = false;
+      },
+
+      notify: function() {
+        return self.newMessages;
+      }
+    });
+  }
+
+  MessageCollection.prototype.push = function(message) {
+    this.newMessages = true;
+    this.messages.push(message);
+  };
+
+  return MessageCollection;
+}]);
