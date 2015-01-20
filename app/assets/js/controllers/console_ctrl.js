718c1aec917e2b04f5c8ababcf0375c0e280bb38
sonnym <michaud.sonny@gmail.com>
2015-01-20 10:34:24 -0500

    remove vestigial console controller

--- app/assets/js/controllers/console_ctrl.js
+++
@@ -1,5 +0,0 @@
-ficsClient.controller("ConsoleCtrl", ["$scope", "Console", function($scope, Console) {
-  $scope.$watch(Console.get, function(output) {
-    $scope.output = output;
-  }, true);
-}]);
