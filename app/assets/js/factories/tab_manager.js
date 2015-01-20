9d7acaa61d525938958349328d009805799ac473
sonnym <michaud.sonny@gmail.com>
2015-01-20 01:06:49 -0500

    simplify tab manager implementation

--- app/assets/js/factories/tab_manager.js
+++ app/assets/js/factories/tab_manager.js
@@ -1,24 +1,17 @@
 ficsClient.factory("TabManager", ["ActivityNotifier", function(ActivityNotifier) {
   return function(scope, tabs) {
-    tabs = _.reduce(tabs, function(memo, data, name) {
+    this.tabs = _.reduce(tabs, function(memo, data, name) {
       memo[name] = new ActivityNotifier(data);
 
       return memo;
     }, {});
-
-    Object.defineProperty(this, "tabs", {
-      enumerable: true,
-      get: function() {
-        return tabs;
-      },
-    });
 
     this.isActiveTab = function(tabName) {
       return tabName === activeTabName();
     };
 
     function activeTabName() {
-      return _.invert(_.reduce(tabs, function(memo, tabData, tabName) {
+      return _.invert(_.reduce(this.tabs, function(memo, tabData, tabName) {
         memo[tabName] = tabData.active;
 
         return memo;
