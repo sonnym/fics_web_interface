74d6e6aeef2652f2eea1f27bc976e3f6c5b9c07d
sonnym <michaud.sonny@gmail.com>
2015-01-26 23:43:29 -0500

    elide boilerplate

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -4,17 +4,10 @@ var browserify = require("browserify");
 var source = require("vinyl-source-stream");
 
 gulp.task("browserify", function(callback) {
-  var bundler = browserify({
+  return browserify({
     entries: ["./app/assets/js/application"],
     debug: true
-  });
-
-  return bundle();
-
-  function bundle() {
-    return bundler
-      .bundle()
-      .pipe(source("application.js"))
-      .pipe(gulp.dest("public/assets"));
-  }
+  }).bundle()
+    .pipe(source("application.js"))
+    .pipe(gulp.dest("public/assets"));
 });
