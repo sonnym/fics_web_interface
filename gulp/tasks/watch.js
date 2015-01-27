7aecff43354f79906276aeb3b6970e010b1f7d52
sonnym <michaud.sonny@gmail.com>
2015-01-27 16:03:52 -0500

    loosen restrictions on extensions

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,7 +1,7 @@
 var gulp = require("gulp");
 
 gulp.task("watch", function() {
-  gulp.watch("app/assets/js/**/*.js", ["scripts"]);
-  gulp.watch("app/assets/css/**/*.css", ["stylesheets"]);
-  gulp.watch("app/assets/templates/**/*.html", ["templates"]);
+  gulp.watch("app/assets/js/**/*.*", ["scripts"]);
+  gulp.watch("app/assets/css/**/*.*", ["stylesheets"]);
+  gulp.watch("app/assets/templates/**/*.*", ["templates"]);
 });
