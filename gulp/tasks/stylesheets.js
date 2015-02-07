5a316b3dd8698b2bd2e7f9dfafddd993371d299c
sonnym <michaud.sonny@gmail.com>
2015-02-07 18:59:52 -0500

    update for node 0.12.0

--- gulp/tasks/stylesheets.js
+++ gulp/tasks/stylesheets.js
@@ -1,8 +1,11 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task("stylesheets", function () {
   gulp.src("app/assets/css/application.scss")
+    .pipe(sourcemaps.init())
     .pipe(sass())
+    .pipe(sourcemaps.write())
     .pipe(gulp.dest("public/assets"));
 });
