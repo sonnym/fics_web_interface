efec6a5876418f2449e8b522118026c9d201c4ba
Sonny Michaud <michaud.sonny@gmail.com>
2015-03-26 21:23:04 -0400

    setup watchify

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,32 +1,42 @@
 "use strict";
 
 var gulp = require("gulp");
+
 var browserify = require("browserify");
 var ngAnnotate = require('gulp-ng-annotate');
 var uglify = require("gulp-uglify");
 
 var sourcemaps = require("gulp-sourcemaps");
+var watchify = require("watchify");
 
 var buffer = require("vinyl-buffer");
 var source = require("vinyl-source-stream");
 
 gulp.task("scripts", ["templates"], function(callback) {
-  return browserify({
-    entries: ["./app/assets/js/application"],
-    debug: true
-  }).bundle()
-    .on("error", function(err) {
-      console.error("\nERROR:\n" + err.message);
+  var bundler = watchify(browserify(watchify.args));
 
-      if (err.annotated) {
-        console.log(err.annotated + "\n");
-      }
-    })
-    .pipe(source("application.js"))
-    .pipe(ngAnnotate())
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(uglify())
-    .pipe(sourcemaps.write("./"))
-    .pipe(gulp.dest("public/assets"));
+  bundler.add("./app/assets/js/application.js");
+
+  bundler.on("update", bundle);
+
+  return bundle();
+
+  function bundle() {
+    return bundler
+      .bundle()
+      .on("error", function(err) {
+        console.error("\nERROR:\n" + err.message);
+
+        if (err.annotated) {
+          console.log(err.annotated + "\n");
+        }
+      })
+      .pipe(source("application.js"))
+      .pipe(ngAnnotate())
+      .pipe(buffer())
+      .pipe(sourcemaps.init({ loadMaps: true }))
+      .pipe(uglify())
+      .pipe(sourcemaps.write("./"))
+      .pipe(gulp.dest("public/assets"));
+  }
 });
